6ba3cb9893dce5fdd0e6e53d1fe61bc9c1db060f
Noushadozi <noushadozi333@gmail.com>
2023-10-05 21:22:45 +0600

    update firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB6q3f9RKsdASFOi9v3mMo0-CGofG7YcIw",
-  authDomain: "dragon-news-auth-af8ca.firebaseapp.com",
-  projectId: "dragon-news-auth-af8ca",
-  storageBucket: "dragon-news-auth-af8ca.appspot.com",
-  messagingSenderId: "413679297276",
-  appId: "1:413679297276:web:6b79f83e123c945ea06d79",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
